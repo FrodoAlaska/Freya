285e3a5fad8ba12ab7231cac8a61315d1d2515ca
Mohamed A.G <moonimooni132@gmail.com>
2026-03-29 05:15:06 +0200

    A few small fixes to the entity module

--- freya/src/entity/entity.cpp
+++ freya/src/entity/entity.cpp
@@ -291,8 +291,7 @@ DynamicBodyComponent& entity_add_dynamic_body(EntityWorld& world,
                                               const OnCollisionFn& enter_func, 
                                               const OnCollisionFn& exit_func, 
                                               const OnCastHitFn& hit_func) {
-  FREYA_DEBUG_ASSERT((desc.type == PHYSICS_BODY_DYNAMIC || desc.type == PHYSICS_BODY_KINEMATIC), 
-                     "Invalid type given to dynamic body component");
+  FREYA_DEBUG_ASSERT((desc.type != PHYSICS_BODY_STATIC), "Invalid type given to dynamic body component");
 
   Transform& transform = world.get<Transform>(entt.get_id());
 
